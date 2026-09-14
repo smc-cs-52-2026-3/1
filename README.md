3fbc3ca7cf4f8e220ada30b30c88c698149472e3
Ellaine Effendi <effendi_ellaine_fa01@student.smc.edu>
2026-09-14 10:06:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CS 52 HW01
+
+### EDIT BELOW
+
+Name: Nathan Greenfield
+
+Email: greenfield_nathan@smc.edu
+
+Platform: MAC
---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2025 Nathan Greenfield. All rights reserved
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	/***************** Your code goes here! *****************/
+
+	// End the program nicely
+	return 0;
+}
